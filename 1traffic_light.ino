63722f43f302f4c93ea5ef87448bf80efee72de5
yusrashabir2006 <yusrashabir2006@gmail.com>
2026-04-05 13:53:54 +0500

    traffic light project
    Initialize traffic light LEDs and implement loop for signaling.

--- 1traffic_light.ino
+++ 1traffic_light.ino
@@ -1,1 +1,27 @@
+int greenled=12;
+int yellowled=11;
+int redled=10;
+void setup()
+{
+  pinMode(greenled, OUTPUT);
+  pinMode(yellowled, OUTPUT);
+  pinMode(redled, OUTPUT);
+}
 
+void loop()
+{
+  digitalWrite(redled,HIGH);
+  digitalWrite(yellowled,LOW);
+  digitalWrite(greenled,LOW);
+   
+  
+  delay(1000);
+  digitalWrite(redled, LOW);
+  digitalWrite(yellowled,HIGH);
+  digitalWrite(greenled,LOW);
+  delay(1000);
+  digitalWrite(redled, LOW);
+  digitalWrite(yellowled,LOW);
+  digitalWrite(greenled,HIGH);
+  delay(1000);
+  
